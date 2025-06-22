72cb8285d28e6db8b16bb9af32a7e16ec482d82d
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-23 00:59:06 +0530

    Update diagnostics.lua

--- lua/core/lsp/diagnostics.lua
+++ lua/core/lsp/diagnostics.lua
@@ -9,12 +9,12 @@ vim.diagnostic.config({
         source = true,
     },
     signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = "󰅚 ",
-            [vim.diagnostic.severity.WARN] = "󰀪 ",
-            [vim.diagnostic.severity.INFO] = "󰋽 ",
-            [vim.diagnostic.severity.HINT] = "󰌶 ",
-        },
+        -- text = {
+        -- [vim.diagnostic.severity.ERROR] = "󰅚 ",
+        -- [vim.diagnostic.severity.WARN] = "󰀪 ",
+        -- [vim.diagnostic.severity.INFO] = "󰋽 ",
+        -- [vim.diagnostic.severity.HINT] = "󰌶 ",
+        -- },
         numhl = {
             [vim.diagnostic.severity.ERROR] = "ErrorMsg",
             [vim.diagnostic.severity.WARN] = "WarningMsg",
