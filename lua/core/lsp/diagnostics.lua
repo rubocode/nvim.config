ca8f9e0f4e12d3c7379e78e0d04de61ab7027d70
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-24 16:12:39 +0530

    toggleterm working!

--- lua/core/lsp/diagnostics.lua
+++ lua/core/lsp/diagnostics.lua
@@ -1,23 +1,23 @@
 vim.diagnostic.config({
-    virtual_lines = true,
-    -- virtual_text = true,
-    underline = true,
-    update_in_insert = true,
-    severity_sort = true,
-    float = {
-        border = "rounded",
-        source = true,
-    },
-    signs = {
-        -- text = {
-        -- [vim.diagnostic.severity.ERROR] = "󰅚 ",
-        -- [vim.diagnostic.severity.WARN] = "󰀪 ",
-        -- [vim.diagnostic.severity.INFO] = "󰋽 ",
-        -- [vim.diagnostic.severity.HINT] = "󰌶 ",
-        -- },
-        numhl = {
-            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-            [vim.diagnostic.severity.WARN] = "WarningMsg",
-        },
-    },
+	virtual_lines = false,
+	-- virtual_text = true,
+	underline = true,
+	update_in_insert = true,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = true,
+	},
+	signs = {
+		-- text = {
+		-- [vim.diagnostic.severity.ERROR] = "󰅚 ",
+		-- [vim.diagnostic.severity.WARN] = "󰀪 ",
+		-- [vim.diagnostic.severity.INFO] = "󰋽 ",
+		-- [vim.diagnostic.severity.HINT] = "󰌶 ",
+		-- },
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+		},
+	},
 })
--- lua/plugins/code/treesitter.lua
+++ lua/plugins/code/treesitter.lua
@@ -1,14 +1,21 @@
 local M = {
 	"nvim-treesitter/nvim-treesitter",
-	branch = 'master', lazy = false, build = ":TSUpdate",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			-- A list of parser names, or "all" (the listed parsers MUST always be installed)
 			ensure_installed = {
-				"c", "lua", 
-				"vim", "vimdoc", "query",
-				"markdown", "markdown_inline",
-				"go", "ruby",
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline",
+				"go",
+				"ruby",
 			},
 
 			-- Install parsers synchronously (only applied to `ensure_installed`)
@@ -33,7 +40,7 @@ local M = {
 				-- list of language that will be disabled
 				disable = { "c", "rust" },
 			},
-		
+
 			-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 			-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 			-- Using this option may slow down your editor, and you may see some duplicate highlights.
