f8c8dedf5232ae8a2068e73be8b87f2c85bfaca3
Ruchira Bomiriya <rubocode@gmail.com>
2025-07-30 21:42:25 +0530

    Update treesitter.lua

--- lua/plugins/code/treesitter.lua
+++ lua/plugins/code/treesitter.lua
@@ -42,6 +42,10 @@ local M = {
 				disable = { "rust" },
 			},
 
+			indent = {
+				enable = true,
+			},
+
 			-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 			-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 			-- Using this option may slow down your editor, and you may see some duplicate highlights.
