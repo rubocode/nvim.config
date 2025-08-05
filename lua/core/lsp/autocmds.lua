7ff8bd3ce3d0fc65f1018575c1b69900693df112
Ruchira Bomiriya <rubocode@gmail.com>
2025-08-05 14:34:55 +0530

    Switched to native autocompletion from blink!

---
+++ lua/core/lsp/autocmds.lua
@@ -0,0 +1,15 @@
+-- Setting up native autocompletion for LSP
+-- AUG 05, 2025 (Marco Peluso)
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method("textDocument/completion") then
+			vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+			vim.keymap.set("i", "<C-Space>", function()
+				vim.lsp.completion.get()
+			end)
+		end
+	end,
+})
--- lua/core/lsp/init.lua
+++ lua/core/lsp/init.lua
@@ -1,3 +1,4 @@
 require("core.lsp.capabilities")
 require("core.lsp.diagnostics")
 require("core.lsp.servers")
+require("core.lsp.autocmds")
