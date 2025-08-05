099e17b24ca06b9540cf5f13f0e1025e9184f135
Ruchira Bomiriya <rubocode@gmail.com>
2025-08-05 14:47:23 +0530

    Update autocmds.lua

--- lua/core/lsp/autocmds.lua
+++ lua/core/lsp/autocmds.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
 		if client then
 			if client:supports_method("textDocument/completion") then
-				vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
+				vim.opt.completeopt = { "menu", "menuone", "noinsert", "noselect", "fuzzy", "popup" }
 				vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
 				vim.keymap.set("i", "<C-Space>", function()
 					vim.lsp.completion.get()
