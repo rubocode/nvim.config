e1712639dd511f4d26ef1cd19e953b6958c99976
Ruchira Bomiriya <ruboware@gmail.com>
2026-08-18 19:51:52 +0530

    Update treesitter.lua

--- lua/plugins/code/treesitter.lua
+++ lua/plugins/code/treesitter.lua
@@ -3,15 +3,9 @@ local M = {
 	branch = "main",
 	lazy = false,
 	build = ":TSUpdate",
-	init = function()
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function()
-				-- Enable treesitter highlighting and disable regex syntax
-				pcall(vim.treesitter.start)
-				-- Enable treesitter-based indentation
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-			end,
-		})
+	config = function()
+		require("nvim-treesitter").setup({})
+		require("nvim-treesitter").install({ "c", "lua", "rust" })
 	end,
 }
 
