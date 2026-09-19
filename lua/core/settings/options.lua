f8879d3efab1cf8b54c8fb572e11449907ba4e26
Ruchira Bomiriya <ruboware@gmail.com>
2026-09-19 12:35:44 +0530

    Update options.lua

--- lua/core/settings/options.lua
+++ lua/core/settings/options.lua
@@ -9,6 +9,8 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
